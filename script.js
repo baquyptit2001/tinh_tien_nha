08b21cc1342fd35ef095df60bcc0a61314e2a4d5
baquyptit2001 <quynb201@gmail.com>
2022-05-10 22:25:50 +0700

    release

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     $('#form_nhap').submit(function (e) {
         e.preventDefault();
-        var so_tien = $('#so_tien').val();
-        var so_nguoi = $('#so_nguoi').val();
-        var so_ngay = $('#so_ngay').val();
+        var so_tien = parseInt($('#so_tien').val());
+        var so_nguoi = parseInt($('#so_nguoi').val());
+        var so_ngay = parseInt($('#so_ngay').val());
         console.log(so_tien, so_nguoi, so_ngay);
-        let res = so_ngay / 30;
-        res = so_tien / ((so_nguoi * 30) + so_ngay) * so_ngay;
+        let tong_ngay = 30 * parseInt(so_nguoi) + parseInt(so_ngay);
+        console.log(tong_ngay);
+        let res = so_tien / tong_ngay;
+        res = res * so_ngay;
         $('#tien_phai_tra').text(res.toLocaleString('en-US'));
     });
 });
