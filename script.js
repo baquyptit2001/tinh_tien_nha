558ef3f71f666f512d817d82de1d27efd6c1ce8d
baquyptit2001 <quynb201@gmail.com>
2022-05-10 22:15:31 +0700

    release

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#form_nhap').submit(function (e) {
+        e.preventDefault();
+        var so_tien = $('#so_tien').val();
+        var so_nguoi = $('#so_nguoi').val();
+        var so_ngay = $('#so_ngay').val();
+        console.log(so_tien, so_nguoi, so_ngay);
+        let res = so_ngay / 30;
+        res = so_tien / (so_nguoi * 30) * so_ngay;
+        $('#tien_phai_tra').text(res.toLocaleString('en-US'));
+    });
+});
